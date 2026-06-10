7adac858a2e7b255b6e78b6a8bdeb12186f97d64
minhtq23bi14286-lgtm <minhtq.23bi14286@usth.edu.vn>
2026-06-10 09:35:37 +0700

    significant changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -28,11 +28,11 @@ const isProduction = process.env.NODE_ENV === 'production';
             ssl: { rejectUnauthorized: false },
           }
         : {
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'Tranminh999!@#',
-            database: 'mobifone_db',
+            host: process.env.DB_HOST || 'localhost',
+            port: Number(process.env.DB_PORT) || 5432,
+            username: process.env.DB_USERNAME || 'postgres',
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE || 'mobifone_db',
           }
       ),
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
